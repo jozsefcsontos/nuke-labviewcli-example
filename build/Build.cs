d3774adc913cf938394f09a13515d05908ae29c1
Jozsef Csontos <jozsef.csontos@ni.com>
2021-06-30 15:11:09 +0200

    Removed comments.

--- build/Build.cs
+++ build/Build.cs
@@ -23,12 +23,6 @@ using static Nuke.Common.Tools.LabVIEWCLI.LabVIEWCLITasks;
 [ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
     public static int Main () => Execute<Build>(x => x.UnitTests);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
